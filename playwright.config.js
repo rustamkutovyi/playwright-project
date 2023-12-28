778123f1e81b02e67597bd20edc16b162a99cc64
rustamkutovyi <rustamkutovoy996@gmail.com>
2023-12-28 13:57:20 -0800

    package.json and playwright.config.js have been updated

--- playwright.config.js
+++ playwright.config.js
@@ -29,16 +29,6 @@ module.exports = defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
   ],
 });
 
