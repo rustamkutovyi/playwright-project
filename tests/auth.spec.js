eb98eb04f741d016ad367219381c40734bb17b12
rustamkutovyi <rustamkutovoy996@gmail.com>
2024-01-02 20:47:45 -0800

    auth tests has been created

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -10,4 +10,16 @@ test.describe('Authentication and Authorization', () => {
 
       await expect(page.locator('.ant-avatar-square')).toBeVisible()
   })
+
+    test('Sign in with not existing credentials', async ({page}) => {
+        await page.goto('https://coding.pasv.us/user/login')
+
+        await page.locator('#normal_login_email').fill('invalid@example.com')
+        await page.locator('#normal_login_password').fill('invalid')
+        await page.locator('button[type="submit"]').click()
+
+        const toast = page.locator('.ant-notification-notice-message')
+        await expect(toast).toBeVisible()
+        await expect(toast).toHaveText('User login. Fail')
+    })
 })
