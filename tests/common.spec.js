2d0cb895c138cb53c8b2ce8564a1af32efab62c3
rustamkutovyi <rustamkutovoy996@gmail.com>
2024-01-04 20:22:27 -0800

    files have been formated by prettier

--- tests/common.spec.js
+++ tests/common.spec.js
@@ -1,25 +1,27 @@
-import { test, expect } from '@playwright/test'
-import {login} from "../common/log-in";
+import {test, expect} from '@playwright/test'
+import {login} from '../common/log-in'
+
 test.describe('Common', () => {
-    test.beforeEach(async ({ page }) => {
-        await login(page, process.env.EMAIL, process.env.PASSWORD)
-    })
+  test.beforeEach(async ({page}) => {
+    await login(page, process.env.EMAIL, process.env.PASSWORD)
+  })
   test('Navigation', async ({page}) => {
+    await page.getByTestId('topmenu-Курсы').click()
+    await expect(page).toHaveURL('/course')
+    await expect(page.getByText('Курсы программирования и тестирования')).toBeVisible()
 
-      await page.getByTestId('topmenu-Курсы').click()
-      await expect(page).toHaveURL('/course')
-      await expect(page.getByText('Курсы программирования и тестирования')).toBeVisible()
+    await page.getByTestId('topmenu-Задачи').click()
+    await expect(page).toHaveURL('/challenge?limit=30&page=1')
+    await expect(page.getByText('Кодинг задачи')).toBeVisible()
 
-      await page.getByTestId('topmenu-Задачи').click()
-      await expect(page).toHaveURL('/challenge?limit=30&page=1')
-      await expect(page.getByText('Кодинг задачи')).toBeVisible()
+    await page.getByTestId('topmenu-Дневник').click()
+    await expect(page).toHaveURL('/diary?page=1')
+    await expect(
+      page.getByText('Дневник успеваемости помогает достигать больших целей')
+    ).toBeVisible()
 
-      await page.getByTestId('topmenu-Дневник').click()
-      await expect(page).toHaveURL('/diary?page=1')
-      await expect(page.getByText('Дневник успеваемости помогает достигать больших целей')).toBeVisible()
-
-      await page.getByTestId('topmenu-Интервью').click()
-      await expect(page).toHaveURL('/flash')
-      await expect(page.getByText('Interview practice cards')).toBeVisible()
+    await page.getByTestId('topmenu-Интервью').click()
+    await expect(page).toHaveURL('/flash')
+    await expect(page.getByText('Interview practice cards')).toBeVisible()
   })
 })
